e34022b0fc1a39a4396e2e438ce2421a411bce9a
Anna <kaletyuk@yandex.ru>
2020-06-23 10:41:58 +0300

    second commit

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const { statement } = require("./script");
+const { statement } = require("./maingit");
 //Первый тест 
 const testInvoises = {
   customer: "MDT",
@@ -80,4 +80,39 @@ test("should return the Error when one of the plays has a wrong type", () => {
 });
 
 //Второй тест
+const testTragedyWithBigAudience = {
+    playId: "Гамлет",
+    audience: 55,
+    type: "tragedy"
+  };
+  
+  const testTragedyWithSmallAudience = {
+    playId: "Гамлет",
+    audience: 20,
+    type: "tragedy"
+  };
+  
+  test("should return the correct number if a tragedy is received for input", () => {
+    expect(addBonuses(0, testTragedyWithBigAudience)).toBe(25);
+    expect(addBonuses(0, testTragedyWithSmallAudience)).toBe(0);
+  });
+  
+  const testComedyWithBigAudience = {
+    playId: "Отелло",
+    audience: 48,
+    type: "comedy"
+  }
+  
+  const testComedyWithSmallAudience = {
+    playId: "Отелло",
+    audience: 20,
+    type: "comedy"
+  }
+  
+  test("should return the correct number if a comedy is received for input", () => {
+    expect(addBonuses(0, testComedyWithBigAudience)).toBe(18);
+    expect(addBonuses(0, testComedyWithSmallAudience)).toBe(0);
+    expect(addBonuses(10, testComedyWithBigAudience)).toBe(27);
+    expect(addBonuses(10, testComedyWithSmallAudience)).toBe(4);
+  });
 
